4c03220d167942361f9ab7e2640098a5f1a4fcdd
Gustavo Leocádio <gleocadio7@gmail.com>
2022-07-31 11:06:31 -0300

    Update IHateJFIF.py

--- IHateJFIF.py
+++ IHateJFIF.py
@@ -2,7 +2,7 @@ import os, sys
 folda = "C:/your/folder/here"
 for file in os.listdir(folda):
     name = os.path.join(folda,file)
-    # print(infilename)
+    # print(name)
     if not os.path.isfile(name): continue
     newname = name.replace('.jfif', '.jpg')
     output = os.rename(name, newname)
